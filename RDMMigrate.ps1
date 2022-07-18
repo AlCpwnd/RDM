e522ab7f525e20e4bf10b0a91a53eb78a9f49844
AlCpwnd <a.dejaeger@hotmail.com>
2022-07-18 17:32:39 +0200

    Continued working on the script
    Added the loop creating a vault for each group found.
    The loop skip if a the vault already exists.

--- RDMMigrate.ps1
+++ RDMMigrate.ps1
@@ -1,5 +1,10 @@
 #Requires -Module RemoteDesktopManager
 
+# Functions:
+function Show-Info{param([String]$Msg)Write-host "$(Get-Date -f HH:mm:ss)`t=i=`t$Msg"}
+function Show-Error{param([String]$Msg)Write-host "$(Get-Date -f HH:mm:ss)`t/!\`t$Msg" -ForegroundColor Yellow}
+
+
 # Connects to the Database configuration
 Start-RDMInstance
 
@@ -9,3 +14,17 @@ $Sessions = Get-RDMSession
 $Groups = $Sessions | Where-Object{$_.Group -notmatch "\\"} | Select-Object Group -Unique 
 # Recovers all existing repositories
 $Repositories = Get-RDMRepository
+
+# Creates a Vault for each group and skips in case it already exists
+foreach($Group in $Groups){
+    if($Repositories.Name -contains $Group){
+        Show-Info "Existing vault found for : $Group .Skipping."
+        Continue
+    }
+    try{
+        $Vault = New-RDMVault -Name $Group
+        Set-RDMVault $Vault -ErrorAction Stop
+    }catch{
+        Show-Error "Failed to create a vault for : $Group"
+    }
+}
