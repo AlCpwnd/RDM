6f3dc7b21b4bddfb2687381e1bf785cb992755c5
AlCpwnd <a.dejaeger@hotmail.com>
2022-08-01 14:32:47 +0200

    Replaced the Repositories commands with vault aliases

--- RDMMigrate.ps1
+++ RDMMigrate.ps1
@@ -40,21 +40,21 @@ function Show-Info{param([String]$Msg)Write-host "$(Get-Date -f HH:mm:ss)`t=i=`t
 function Show-Error{param([String]$Msg)Write-host "$(Get-Date -f HH:mm:ss)`t/!\`t$Msg" -ForegroundColor Yellow}
 
 # Verifies the parameter
-$MVInfo = Get-RDMRepository -Name $MainVault
+$MVInfo = Get-RDMVault -Name $MainVault
 if(!$MVInfo){
     Show-Error "Invalid Main Vault : $MainVault"
     return
 }
 
 # Sets current location within the $MainVault
-Set-RDMCurrentRepository $MVInfo
+Set-RDMCurrntVault $MVInfo
 
 # Recovers all existing cesisons
 $Sessions = Get-RDMSession 
 # Recovers all groups
 $Groups = $Sessions | Where-Object{$_.Group -notmatch "\\"} | Select-Object Group -Unique 
 # Recovers all existing repositories
-$Repositories = Get-RDMRepository
+$Repositories = Get-RDMVault
 
 # Creates a Vault for each group and skips in case it already exists
 foreach($Group in $Groups){
@@ -80,7 +80,7 @@ foreach($Session in $Sessions){
         Continue
     }
     # Moves to the $MainVault
-    Set-RDMCurrentRepository $MVInfo
+    Set-RDMCurrntVault $MVInfo
     try{
         # Recovers the information of the current cession
         # -DontChangeID  -->  Will move instead of copy the cession
@@ -88,7 +88,7 @@ foreach($Session in $Sessions){
         # Recovers the new location's information based on the cession's name
         $NewVault = Get-RDMVault -Name $Session.Split("\")[0]
         # Moves into the new vault        
-        Set-RDMCurrentRepository $NewVault
+        Set-RDMCurrntVault $NewVault
         # Copies the cession over
         Set-RDMCession $Move
     }catch{
