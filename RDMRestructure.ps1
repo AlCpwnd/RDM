633e876be2443c14eb759e97c36ab747ce34b07b
AlCpwnd <a.dejaeger@hotmail.com>
2023-01-26 15:43:17 +0100

    Started re-writing the script due to change requests

--- RDMRestructure.ps1
+++ RDMRestructure.ps1
@@ -53,64 +53,64 @@ function Test-Vault{
     return $VaultInfo
 }
 
-function DrawMenu {
-    param ($menuItems, $menuPosition, $Multiselect, $selection)
+function DrawMenu{
+    param($menuItems, $menuPosition, $Multiselect, $selection)
     $l = $menuItems.length
-    for ($i = 0; $i -le $l;$i++) {
-		if ($menuItems[$i] -ne $null){
+    for($i = 0; $i -le $l;$i++){
+		if($null -ne $menuItems[$i]){
 			$item = $menuItems[$i]
-			if ($Multiselect)
+			if($Multiselect)
 			{
-				if ($selection -contains $i){
+				if($selection -contains $i){
 					$item = '[x] ' + $item
 				}
-				else {
+				else{
 					$item = '[ ] ' + $item
 				}
 			}
-			if ($i -eq $menuPosition) {
+			if($i -eq $menuPosition){
 				Write-Host "> $($item)" -ForegroundColor Green
-			} else {
+			}else{
 				Write-Host "  $($item)"
 			}
 		}
     }
 }
 
-function Toggle-Selection {
-	param ($pos, [array]$selection)
-	if ($selection -contains $pos){ 
-		$result = $selection | where {$_ -ne $pos}
+function Toggle-Selection{
+	param($pos, [array]$selection)
+	if($selection -contains $pos){ 
+		$result = $selection | Where-Object{$_ -ne $pos}
 	}
-	else {
+	else{
 		$selection += $pos
 		$result = $selection
 	}
 	$result
 }
 
-function Menu {
-    param ([array]$menuItems, [switch]$ReturnIndex=$false, [switch]$Multiselect)
+function Menu{
+    param([array]$menuItems, [switch]$ReturnIndex=$false, [switch]$Multiselect)
     $vkeycode = 0
     $pos = 0
     $selection = @()
-    if ($menuItems.Length -gt 0)
+    if($menuItems.Length -gt 0)
 	{
-		try {
+		try{
 			[console]::CursorVisible=$false #prevents cursor flickering
 			DrawMenu $menuItems $pos $Multiselect $selection
-			While ($vkeycode -ne 13 -and $vkeycode -ne 27) {
+			While($vkeycode -ne 13 -and $vkeycode -ne 27){
 				$press = $host.ui.rawui.readkey("NoEcho,IncludeKeyDown")
 				$vkeycode = $press.virtualkeycode
-				If ($vkeycode -eq 38 -or $press.Character -eq 'k') {$pos--}
-				If ($vkeycode -eq 40 -or $press.Character -eq 'j') {$pos++}
-				If ($vkeycode -eq 36) { $pos = 0 }
-				If ($vkeycode -eq 35) { $pos = $menuItems.length - 1 }
-				If ($press.Character -eq ' ') { $selection = Toggle-Selection $pos $selection }
-				if ($pos -lt 0) {$pos = 0}
-				If ($vkeycode -eq 27) {$pos = $null }
-				if ($pos -ge $menuItems.length) {$pos = $menuItems.length -1}
-				if ($vkeycode -ne 27)
+				If($vkeycode -eq 38 -or $press.Character -eq 'k'){$pos--}
+				If($vkeycode -eq 40 -or $press.Character -eq 'j'){$pos++}
+				If($vkeycode -eq 36){ $pos = 0 }
+				If($vkeycode -eq 35){ $pos = $menuItems.length - 1 }
+				If($press.Character -eq ' '){ $selection = Toggle-Selection $pos $selection }
+				if($pos -lt 0){$pos = 0}
+				If($vkeycode -eq 27){$pos = $null }
+				if($pos -ge $menuItems.length){$pos = $menuItems.length -1}
+				if($vkeycode -ne 27)
 				{
 					$startPos = [System.Console]::CursorTop - $menuItems.Length
 					[System.Console]::SetCursorPosition(0, $startPos)
@@ -118,30 +118,30 @@ function Menu {
 				}
 			}
 		}
-		finally {
+		finally{
 			[System.Console]::SetCursorPosition(0, $startPos + $menuItems.Length)
 			[console]::CursorVisible = $true
 		}
 	}
-	else {
+	else{
 		$pos = $null
 	}
 
-    if ($ReturnIndex -eq $false -and $pos -ne $null)
+    if($ReturnIndex -eq $false -and $null -ne $pos)
 	{
-		if ($Multiselect){
+		if($Multiselect){
 			return $menuItems[$selection]
 		}
-		else {
+		else{
 			return $menuItems[$pos]
 		}
 	}
 	else 
 	{
-		if ($Multiselect){
+		if($Multiselect){
 			return $selection
 		}
-		else {
+		else{
 			return $pos
 		}
 	}
@@ -164,7 +164,15 @@ try{
     show-error "Couldn't recover the existing vaults. Exiting"
     return
 }
-switch(menu "List existing vaults","Enter the vault Name from the source vault"){
-    "List existing vaults" {$Vaults.Name}
-    "Enter the vault Name from the source vault" {$SourceVault = Read-Host -Prompt "Vault Name:"}
+
+$SVInfo = $false
+
+while(!$SVInfo){
+	switch(menu "List existing vaults","Enter the vault Name from the source vault","/!\Quit"){
+		"List existing vaults"{$Vaults.Name}
+		"Enter the vault Name from the source vault"{$SourceVault = Read-Host -Prompt "Vault Name:"}
+		"/!\Quit" {return}
+	}
+	$SVInfo = Test-Vault -Vault $SourceVault
 }
+
