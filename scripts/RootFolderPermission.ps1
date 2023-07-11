3f604c6e6bcfb74a6af64b621ea53f7e12e91124
AlCpwnd <a.dejaeger@hotmail.com>
2023-07-11 13:14:05 +0200

    Additionnal comments and clarifications

--- scripts/RootFolderPermission.ps1
+++ scripts/RootFolderPermission.ps1
@@ -6,12 +6,15 @@
 # Recovering the template's permissions.
 Set-RDMCurrentRepository (Get-RDMRepository -Name 'Template_Vault')
 $RootTemplate = Get-RDMRootSession
+$Properties = $RootTemplate.Security.PSObject.Properties
 
 # Recovering existing vaults.
-$Properties = $RootTemplate.Security.PSObject.Properties
 $Vaults = Get-RDMRepository
 foreach($Vault in $Vaults){
+    # Moving to the concerned vault.
     Set-RDMCurrentRepository $Vault
+
+    # Applying the permissions on the root folder.
     $RootFolder = Get-RDMRootSession
     $Properties | ForEach-Object{$RootFolder.Security.$($_.Name) = $_.Value}
     Set-RDMRootSession $RootFolder
